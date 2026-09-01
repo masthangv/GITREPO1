579650da8531725a7fb9d23b6f58c7fbd0835eb8
masthangv <123943477+masthangv@users.noreply.github.com>
2026-09-01 19:39:52 +0530

    Revert "Update sample1.java"

--- sample1.java
+++ sample1.java
@@ -1,6 +1,6 @@
 // Main.java
 public class Main {
-    public static void main {
+    public static void main(String[] args) {
         // System.out.println() prints text to the console
         System.out.println("Hello, World!");
     }
